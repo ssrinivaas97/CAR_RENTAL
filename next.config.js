318cf06d7d990252177383be748302e06e50bde4
Sunilsrinivaasneu <122786980+ssrinivaas97@users.noreply.github.com>
2023-07-07 23:35:12 -0700

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['cdn.imagin.studio']
+        domains: ['cdn.imagin.studio'],
+        output: 'export',
     }
 }
 
